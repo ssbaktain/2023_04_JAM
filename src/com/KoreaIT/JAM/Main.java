6a4b018bfb122779c81fd6e5f911318a8913306b
ssbaktain <ssbaktain12@gmail.com>
2023-04-27 19:39:02 +0900

    article write, article list 기능 구현

--- src/com/KoreaIT/JAM/Main.java
+++ src/com/KoreaIT/JAM/Main.java
@@ -1,10 +1,15 @@
 package com.KoreaIT.JAM;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		
+		List<Article> articles = new ArrayList<>();
+		int lastArticleId = 0;
 		
 		System.out.println("== 프로그램 시작 ==");
 		
@@ -16,7 +21,32 @@ public class Main {
 				System.out.println("== 프로그램 끝 ==");
 				break;
 			}
+			
+			if (cmd.equals("article write")) {
+				System.out.println("== 게시물 작성 ==");
+				int id = ++lastArticleId;
+				System.out.print("제목 : ");
+				String title = sc.nextLine();
+				System.out.print("내용 : ");
+				String body = sc.nextLine();
+				
+				articles.add(new Article(id, title, body));
+				System.out.printf("%d번 글이 생성되었습니다.\n", id);
+			} else if (cmd.equals("article list")) {
+				System.out.println("== 게시물 리스트 ==");
+				
+				if (articles.size() == 0) {
+					System.out.println("존재하는 게시물이 없습니다.");
+					continue;
+				}
+				
+				System.out.printf("번호	|	제목\n");
+				for (Article article : articles) {
+					System.out.printf("%d	|	%s\n", article.id, article.title);
+				}
+			}
 		}
 		sc.close();
 	}
+	
 }
